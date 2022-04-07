fe7de374ac604006942f1b3d70f12f121b592824
Jaycob Duffel <48539549+JaycobDuffel@users.noreply.github.com>
2022-04-06 21:59:22 -0700

    fixed view and db (#5)

--- api/tasky_api/tasky_api/serializers/project.py
+++ api/tasky_api/tasky_api/serializers/project.py
@@ -4,7 +4,6 @@ from tasky_api.models import Project
 from tasky_api.serializers.task import TaskSerializer
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
-    task = TaskSerializer(many=True, read_only=True)
     class Meta:
         model = Project
         fields = ('pk', 'title', 'description')
