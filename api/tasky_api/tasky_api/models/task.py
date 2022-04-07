4448532fffb854a9b82bf239e88003bf3c36e648
Jaycob <jaycobduffel@gmail.com>
2022-04-06 17:17:42 -0700

    Fix Migrations + Task model

--- api/tasky_api/tasky_api/models/task.py
+++ api/tasky_api/tasky_api/models/task.py
@@ -1,10 +1,12 @@
 from django.db import models
 from .project import Project
+from .stage import Stage
 
 class Task(models.Model):
     identifier = models.CharField(max_length=20, unique=True)
     description = models.TextField(blank=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True)
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.identifier
