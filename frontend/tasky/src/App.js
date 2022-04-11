912574de310d750f2a63850cd7b171addb2d4e5e
Jaycob <jaycobduffel@gmail.com>
2022-04-11 16:39:40 -0700

    Create and use base container component

--- frontend/tasky/src/App.js
+++ frontend/tasky/src/App.js
@@ -1,19 +1,13 @@
 import ApiService from "./api/baseAPI";
 import './App.css';
+import Container from "./components/Container";
 
 function App() {
 
   const api = new ApiService();
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={() => {
-          api.createStage({
-          title: 'In Progress',
-          order: 2,
-          project: 1,
-        })}}>Add Project</button>
-      </header>
+      <Container />
     </div>
   );
 }
---
+++ frontend/tasky/src/components/Container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Container = () => {
+return (
+  <div style={{height: '90%', width: '100%'}}>
+    <h1>Container</h1>
+  </div>
+)
+}
+
+export default Container
