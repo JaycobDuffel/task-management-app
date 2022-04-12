27a91d9de1acd7079402cea2aa09ca738884a6d0
Jaycob Duffel <48539549+JaycobDuffel@users.noreply.github.com>
2022-04-12 09:10:00 -0700

    fix project rel in stage model (#8)

--- api/tasky_api/tasky_api/models/stage.py
+++ api/tasky_api/tasky_api/models/stage.py
@@ -4,7 +4,7 @@ from .project import Project
 class Stage(models.Model):
     title = models.CharField(max_length=30)
     order = models.IntegerField(blank=True)
-    projects = models.ManyToManyField(Project, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.title
